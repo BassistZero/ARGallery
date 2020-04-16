58d0b603ea32c0e0ee04ed46571ee8b5674ddec9
BassistZero <46396970+BassistZero@users.noreply.github.com>
2020-04-16 16:49:09 +0300

    Initial Commit

---
+++ ARGallery/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ARGallery
+//
+//  Created by Bassist Zero on 4/16/20.
+//  Copyright © 2020 Bassist_Zero. All rights reserved.
+//
+
+import UIKit
+import RealityKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var arView: ARView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Load the "Box" scene from the "Experience" Reality File
+        let boxAnchor = try! Experience.loadBox()
+        
+        // Add the box anchor to the scene
+        arView.scene.anchors.append(boxAnchor)
+    }
+}
