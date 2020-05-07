255e4aa8d797ae68dd7b54cba1a83d8fa6fd2c8b
Bassist Zero <bassist.zero.0@gmail.com>
2020-05-07 15:20:33 +0300

    needa fix imagePickerController and addPainting

--- ARGallery/ViewController.swift
+++ ARGallery/ViewController.swift
@@ -15,7 +15,7 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     
     @IBOutlet var arView: ARSCNView!
     
-    @IBOutlet weak var imageValue: UIImageView!
+    @IBOutlet weak var imageValueUIView: UIImageView!
     
     
     @IBAction func takePhotoButton(_ sender: Any) {
@@ -30,55 +30,132 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         self.present(imagePicker, animated: true, completion: nil)
     }
     
-    @IBAction func randomPic(_ sender: Any) {
-        let service = BaseService()
-
-        service.loadPhotos(onComplete: { [weak self] (photo) in
-            self?.photo = photo
-            self?.imageValue.image = self?.image
-            }
-            , onError: { error in
-            dump(error)
-            })
-    }
-    
     
     var grids = [Grid]()
     var image: UIImage?
     var photo: PhotoModel?
+    var imageValue: UIImage? = UIImage(named: "mona-lisa")!
+    
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // Create a session configuration
+        let configuration = ARWorldTrackingConfiguration()
+        configuration.planeDetection = .vertical
+
+        // Run the view's session
+        arView?.session.run(configuration)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        // Pause the view's session
+        arView?.session.pause()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        //let scene = SCNScene()
+        arView?.delegate = self
+        arView?.debugOptions = ARSCNDebugOptions.showFeaturePoints
         
-        //ARView.Image = imageValue
+        let scene = SCNScene()
         
-        //let anchor = AnchorEntity(plane: .vertical)
-        //arView?.scene.addAnchor(anchor)
+        arView?.scene = scene
         
-        //var ARFrame: Entity = try! ARScene.loadBox().frameAndImage!
-        
-        //anchor.addChild(ARFrame)
-        
-        
-        //let anchor = try? ARScene.loadBox()
-        //arView?.scene.anchors.append(anchor!)
+        let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(tapped))
+        arView?.addGestureRecognizer(gestureRecognizer)
     }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Release any cached data, images, etc that aren't in use.
+    }
+    
    
     let imagePicker = UIImagePickerController()
     
-    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any],_ imageValue: inout UIImage?) {
         let image = info[UIImagePickerController.InfoKey.originalImage] as! UIImage
-        imageValue.image = image
+        imageValue = image
         picker.dismiss(animated: true, completion: nil)
     }
  
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        guard let planeAnchor = anchor as? ARPlaneAnchor, planeAnchor.alignment == .vertical else { return }
+        let grid = Grid(anchor: planeAnchor)
+        grids.append(grid)
+        node.addChildNode(grid)
+    }
+
+    func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
+        guard let planeAnchor = anchor as? ARPlaneAnchor, planeAnchor.alignment == .vertical else { return }
+        let grid = self.grids.filter { grid in
+            return grid.anchor.identifier == planeAnchor.identifier
+            }.first
+        
+        guard let foundGrid = grid else {
+            return
+        }
+        
+        foundGrid.update(anchor: planeAnchor)
+    }
+
+    @objc func tapped(gesture: UITapGestureRecognizer) {
+           // Get 2D position of touch event on screen
+           let touchPosition = gesture.location(in: arView)
+           
+           // Translate those 2D points to 3D points using hitTest (existing plane)
+           let hitTestResults = arView.hitTest(touchPosition, types: .existingPlaneUsingExtent)
+           
+           // Get hitTest results and ensure that the hitTest corresponds to a grid that has been placed on a wall
+        guard let hitTest = hitTestResults.first, let anchor = hitTest.anchor as? ARPlaneAnchor, let gridIndex = grids.firstIndex(where: { $0.anchor == anchor }) else {
+               return
+           }
+        addPainting(hitTest, grids[gridIndex], imageValue: &imageValue)
+       }
+
+    func addPainting(_ hitResult: ARHitTestResult, _ grid: Grid, imageValue: inout UIImage?) {
+        // 1.
+        let planeGeometry = SCNPlane(width: 0.2, height: 0.35)
+        let material = SCNMaterial()
+        material.diffuse.contents = imageValue
+        planeGeometry.materials = [material]
+        
+        // 2.
+        let paintingNode = SCNNode(geometry: planeGeometry)
+        paintingNode.transform = SCNMatrix4(hitResult.anchor!.transform)
+        paintingNode.eulerAngles = SCNVector3(paintingNode.eulerAngles.x + (-Float.pi / 2), paintingNode.eulerAngles.y, paintingNode.eulerAngles.z)
+        paintingNode.position = SCNVector3(hitResult.worldTransform.columns.3.x, hitResult.worldTransform.columns.3.y, hitResult.worldTransform.columns.3.z)
+        
+        arView.scene.rootNode.addChildNode(paintingNode)
+        grid.removeFromParentNode()
+    }
 }
 
 extension UIImageView {
     func loadImage(by imageURL: String) {
         let url = URL(string: imageURL)!
-        let data = try! Data(contentsOf: url)
-        self.image = UIImage(data: data)
+        
+        let cache = URLCache.shared
+        let request = URLRequest(url: url)
+        
+        if let imageData = cache.cachedResponse(for: request)?.data {
+            self.image = UIImage(data: imageData)
+        } else {
+            URLSession.shared.dataTask(with: request) { (data, response, _) in
+                DispatchQueue.main.async {
+                    guard let data = data, let response = response else {
+                        return
+                    }
+                    let cacheResponse = CachedURLResponse(response: response, data: data)
+                }
+            }.resume()
+        }
+        
+        //let data = try! Data(contentsOf: url)
+        //self.image = UIImage(data: data)
     }
 }
